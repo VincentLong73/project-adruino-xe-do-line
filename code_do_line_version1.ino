983297ee15fcd6a652c9a198b456d1adb638bb89
Nguyễn Uyên <55393354+NguyenUyen0904@users.noreply.github.com>
2021-06-08 05:24:24 +0700

    update ver1_ chay hinh chu S
    chạy hình chữ S từ ver1

--- code_do_line_version1.ino
+++ code_do_line_version1.ino
@@ -1,79 +1,86 @@
-byte SpMaxL=100;
-byte SpMaxR=100;
+//đường đen là chữ S 
+// IR Sensors_ Khai báo 5 chân của sensor 
+int sensor1 = A0;      // Left most sensor
+int sensor2 = A1;
+int sensor3 = A2;
+int sensor4 = A3;      
+int sensor5 = A4;      // Right most sensor
+// Initial Values of Sensors_ Khai báo mảng giá trị của sensor
+int sensor[5] = {0, 0, 0,0 , 0};
 
+// Motor Variables_ Khai báo 4 chân động cơ và 2 chân điều khiển 
+int ENA = 6;              //Motor Trái
+int motorInput1 = 7;
+int motorInput2 = 8;
+int motorInput3 = 9;
+int motorInput4 = 10;
+int ENB = 11; 
+byte SpMaxL=200;
+byte SpMaxR=200;
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(10, OUTPUT);//Set chân in1 của dc A là output
-  pinMode(11, OUTPUT);//Set chân in2 của dc A là output
-  pinMode(7, OUTPUT);//Set chân in1 của dc A là output
-  pinMode(6, OUTPUT);//Set chân in2 của dc A là output
-  pinMode(5, OUTPUT);//Set chân in1 của dc B là output
-  pinMode(4, OUTPUT);//Set chân in2 của dc B là output
-  pinMode(A0, INPUT);//Set chân cảm biến 1 là input
-  pinMode(A1, INPUT);//Set chân cảm biến 2 là input
-  pinMode(A2, INPUT);//Set chân cảm biến 3 là input
-  pinMode(A3, INPUT);//Set chân cảm biến 4 là input
-  pinMode(A4, INPUT);//Set chân cảm biến 4 là input
- // analogWrite(10, 200);
-  //analogWrite(11, 200);
+void setup()
+{
+  pinMode(sensor1, INPUT);
+  pinMode(sensor2, INPUT);
+  pinMode(sensor3, INPUT);
+  pinMode(sensor4, INPUT);
+  pinMode(sensor5, INPUT);
+
+  pinMode(motorInput1, OUTPUT);
+  pinMode(motorInput2, OUTPUT);
+  pinMode(motorInput3, OUTPUT);
+  pinMode(motorInput4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  
+  Serial.begin(9600);                     //setting serial monitor at a default baund rate of 9600
+  delay(500);
+  // Serial.println("Started !!");
+  // delay(1000);
 }
 
-
 void loop() {
-  //Tien();
-  //Lui();
-  //Retrai();
-  //Rephai();
+  
   doline();
-  //delay (1000);
-  //Serial.println (trangThai5CamBien ());
-  //delay(1000);
+ 
 }
 
 void doline()
 {
   switch (trangThai5CamBien())
   {
-    case 200:
-      //Tien(SpMaxL,SpMaxR-150);
-      Lui(100,100);
-      //Rephai();
-      delay(10);
+    case 200:// đã đi hết line chữ S  
+      
+      Tien(SpMaxL,SpMaxR-40);
+      
       break;
     case 0:
       Tien(SpMaxL,SpMaxR);
-      delay(10);
       break;
     case 1:
       Tien(SpMaxL,SpMaxR-20);
-      delay(10);
       break;
     case 2:
       Tien(SpMaxL,SpMaxR-40);
-      delay(10);
       break;
     case 3:
       Tien(SpMaxL,0);
-      delay(10);
       break;
     case -1:
       Tien(SpMaxL-20,SpMaxR);
-      delay(10);
       break;
     case -2:
       Tien(SpMaxL-40,SpMaxR);
-      delay(10);
       break;
     case -3:
       Tien(0,SpMaxR);
-      delay(10);
       break;
-    default:
-      digitalWrite(6, 0);
-      digitalWrite(7, 0);
-      digitalWrite(8, 0);
-      digitalWrite(9, 0);
+    default:// mặc định thì dừng
+      digitalWrite(motorInput1, 0);
+      digitalWrite(motorInput2, 0);
+      digitalWrite(motorInput3, 0);
+      digitalWrite(motorInput, 0);
       break;
   }
 }
@@ -83,7 +90,7 @@ boolean IFSensor (byte PinNumb)
   return (!digitalRead (PinNumb));
 }
 
-int trangThai5CamBien ()// biến lưu trạng thái cảm biến 5 mắt 
+int trangThai5CamBien ()
 {
   /*
       Âm là lệch phải
@@ -97,16 +104,16 @@ int trangThai5CamBien ()// biến lưu trạng thái cảm biến 5 mắt
   int sens5 = IFSensor(A4);
   switch (lechvach5mat())
   {
-    case -3:// 2 ss trái s1, s2 nhận dc tín hiệu 
+    case -3:
       return -3;
       break;
-    case -2:// cái cảm biến S1 nhận được tín hiệu  
+    case -2:
       return -2;
       break;
-    case -1:// cái cảm biến S2 nhận được tín hiệu
+    case -1:
       return -1;
       break;
-    case 0:// ko cái nào nhận được thông tin đường 
+    case 0:
       if ( (abs(sens1) + abs(sens2) + abs(sens3) + abs(sens4) + abs(sens5)) == 0)
         return 200;
       else
@@ -139,16 +146,16 @@ int lechvach5mat()
   int lechvach = -2 * sens1 - sens2 + sens4 + 2 * sens5;
   switch (lechvach)
   {
-    case -3:// cái này là do 2 mắt s1, s2 biên đều chạm vạch
+    case -3:// cái này là do 2 mắt biên đều chạm vạch
       return -2;
       break;
-    case -2: // s1 chạm vạch 
+    case -2:
       return -3;
       break;
-    case -1://s2 chạm vạch 
+    case -1:
       return -1;
       break;
-    case 0:// s3 chạm vạch , đi đúng đường 
+    case 0:
       return 0;
       break;
     case 1:
@@ -166,37 +173,39 @@ int lechvach5mat()
 }
 void Tien( byte speedL, byte speedR)
 {
-  analogWrite(11, speedL);
-  analogWrite(10, speedR);
-  digitalWrite(7, 1);
-  digitalWrite(6, 0);
-  digitalWrite(5, 1);
-  digitalWrite(4, 0);
+  analogWrite(ENA, speedL);
+  analogWrite(ENB, speedR);
+  digitalWrite(motorInput1, 1);
+  digitalWrite(motorInput2, 0);
+  digitalWrite(motorInput3, 1);
+  digitalWrite(motorInput4, 0);
 }
-void Retrai( byte speedL, byte speedR)
+void Retrai()
 {
-  analogWrite(11, speedL);
-  analogWrite(10, speedR);
-  digitalWrite(7, 0);
-  digitalWrite(6, 0);
-  digitalWrite(5, 1);
-  digitalWrite(4, 0);
+  analogWrite(ENA, 255);
+  analogWrite(ENB, 255);
+  digitalWrite(motorInput1, 0);
+  digitalWrite(motorInput2, 0);
+  digitalWrite(motorInput3, 1);
+  digitalWrite(motorInput4, 0);
 }
-void Rephai( byte speedL, byte speedR)
+void Rephai()
 {
-  analogWrite(11, speedL);
-  analogWrite(10, speedR);
-  digitalWrite(7, 1);
-  digitalWrite(6, 0);
-  digitalWrite(5, 0);
-  digitalWrite(4, 0);
+  analogWrite(5, 255);
+  analogWrite(10, 255);
+  analogWrite(ENA, 255);
+  analogWrite(ENB, 255);
+  digitalWrite(motorInput1, 1);
+  digitalWrite(motorInput2, 0);
+  digitalWrite(motorInput3, 0);
+  digitalWrite(motorInput4, 0);
 }
 void Lui(byte SpeedL, byte SpeedR)
 {
-  analogWrite(11, SpeedL);
-  analogWrite(10, SpeedR);
-  digitalWrite(7, 0);
-  digitalWrite(6, 1);
-  digitalWrite(5, 0);
-  digitalWrite(4, 1);
+  analogWrite(ENA, speedL);
+  analogWrite(ENB, speedR);
+  digitalWrite(motorInput1, 0);
+  digitalWrite(motorInput2, 1);
+  digitalWrite(motorInput3, 0);
+  digitalWrite(motorInput4, 1);
 }
